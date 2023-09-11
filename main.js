d1da42747ffce673bc442a2bb0ff051e0752aaac
Arturo <arturcas2@hotmail.com>
2023-09-10 20:17:20 -0600

    adding random button

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ import { getCharacters } from './src/mortyRick/rickMorty';
 document.querySelector('#app').innerHTML = `
   <div>
     <h1 id="app-title">The Rick and Morty👽</h1>
-    <input id="search" placeholder="Search..."/>
+    <input id="search" placeholder="Search..."/> <br/>
+    <button id="btn-random">Randoms...</button>
     <div class="card"></div>
   </div>
 `
